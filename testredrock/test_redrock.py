565b331ebdd4a2e9836eefb4720625975d084cdf
szstonelee <szstonelee@vip.qq.com>
2020-06-23 10:57:15 -0700

    simplify the files in root folder

--- testredrock/test_redrock.py
+++ testredrock/test_redrock.py
@@ -475,10 +475,10 @@ def _check_lfu_for_eviction():
 
 
 def _main():
-    _warm_up_with_string()
-    _check_all_key_in_string()
-    #_warm_up_with_all_data_types()
-    #_check_all_key_in_data_types()
+    #_warm_up_with_string()
+    #_check_all_key_in_string()
+    _warm_up_with_all_data_types()
+    _check_all_key_in_data_types()
     #_check_pipeline()
     #_warm_up_for_block()
     #_check_block()
